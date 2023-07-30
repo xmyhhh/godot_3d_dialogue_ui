1c1420f4559ea0627fc072a6b4c3b4dc8c12a8dc
xmyhhh <xmycissy@outlook.com>
2023-07-30 21:05:29 +0800

    update

---
+++ Camera3D.gd
@@ -0,0 +1,60 @@
+# Godot Sponza: Camera freelook and movement script
+#
+# Copyright © 2017-2021 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends Camera3D
+
+const MOUSE_SENSITIVITY = 0.002
+
+
+
+# The camera movement speed (tweakable using the mouse wheel)
+var move_speed := 0.5
+
+# Stores where the camera is wanting to go (based on pressed keys and speed modifier)
+
+
+# Stores the effective camera velocity
+var velocity := Vector3()
+
+# The initial camera node rotation
+var initial_rotation := rotation.y
+
+
+func _input(event: InputEvent) -> void:
+	# Mouse look (effective only if the mouse is captured)
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		# Horizontal mouse look
+		rotation.y -= event.relative.x * MOUSE_SENSITIVITY
+		# Vertical mouse look, clamped to -90..90 degrees
+		rotation.x = clamp(rotation.x - event.relative.y * MOUSE_SENSITIVITY, deg_to_rad(-90), deg_to_rad(90))
+
+
+
+func _process(delta: float) -> void:
+	var motion := Vector3()
+	if Input.is_key_label_pressed(KEY_W):
+		motion.z = -1
+	elif Input.is_key_label_pressed(KEY_S):
+		motion.z = 1
+	else:
+		motion.z = 0
+		
+	if Input.is_key_label_pressed(KEY_A):
+		motion.x = -1
+	elif Input.is_key_label_pressed(KEY_D):
+		motion.x = 1
+	else:
+		motion.x = 0
+
+	if Input.is_key_label_pressed(KEY_Q):
+		motion.y = 1
+	elif Input.is_key_label_pressed(KEY_E):
+		motion.y = -1
+	else:
+		motion.y = 0
+
+	transform.origin += motion * delta
+
+
--- Label3D.gd
+++ Label3D.gd
@@ -1,18 +1,15 @@
 
 extends Label3D
- 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(self.get_aabb().size)
-	var childnode = get_children()[0]
-	childnode.scale.x = self.get_aabb().size.x /5.12  + 0.3
-	childnode.scale.y = self.get_aabb().size.y /2.56 + 0.2
-	var mat = childnode.material_override
-	mat.set_shader_parameter("size", Vector2(childnode.scale.x * 100, childnode.scale.y * 100))
+	pass
+	var cur_cam =  get_tree().get_root().get_viewport().get_camera_3d() 
+	var dir = cur_cam.global_transform.origin 
+	look_at(dir, cur_cam.global_transform.basis.y, true)
+#
+#	var childnode = get_children()[0]
+#	childnode.scale.x = self.get_aabb().size.x /5.12  + 0.3
+#	childnode.scale.y = self.get_aabb().size.y /2.56 + 0.2
+#	var mat = childnode.material_override
+#	mat.set_shader_parameter("size", Vector2(childnode.scale.x * 100, childnode.scale.y * 100))
 
