de6409e9c855220b4fe57ecc676f5bd877c170bd
xmyhhh <xmycissy@outlook.com>
2023-07-31 16:01:24 +0800

    update

--- smart_label_bg.gd
+++ smart_label_bg.gd
@@ -1,22 +1,24 @@
-@tool
-extends Label3D
 
+extends Node3D
+
+@onready var origin_offset = Vector3(0, 0, 0)
 
 func _process(delta):
+	origin_offset = transform.origin + Vector3(0, 2, 0)
+	
+	var label_node = get_child(0)
+	var label_pixel_size = label_node.get_aabb().size / self.pixel_size
+	label_pixel_size.x += 60
+	label_pixel_size.y += 60
+	self.texture.width = label_pixel_size.x
+	self.texture.height = label_pixel_size.y
+	self.material_override.set_shader_parameter("size", label_pixel_size)
+#	self.material_override.set_shader_parameter("size", Vector2(5.12 * 100, 2.56 * 100))
+	self.material_override.set_shader_parameter("origin_offset", origin_offset)
+	var cur_cam = get_tree().get_root().get_viewport().get_camera_3d()
+	var MODEL_MATRIX = global_transform
+	var VIEW_MATRIX = cur_cam.global_transform
+	var offset_dir = origin_offset * MODEL_MATRIX - Vector3(0, 0, 0) * MODEL_MATRIX
+	label_node.global_transform.origin = global_transform.origin - offset_dir ;
+	label_node.global_transform.origin =  ( label_node.global_transform.origin * VIEW_MATRIX + Vector3(origin_offset.x, origin_offset.y, 0)) * VIEW_MATRIX.inverse() 
 
-	var all_child = get_children()
-
-	for child in all_child:
-		var scale  = Vector2(self.get_aabb().size.x /5.12  + 0.3, self.get_aabb().size.y /2.56 + 0.2)
-		child.get_children()[0].material_override.set_shader_parameter("size", Vector2(scale.x * 100, scale.y * 100))
-		
-		
-		
-#		print(child.transform.origin)
-#	look_at(dir, cur_cam.global_transform.basis.y, true)
-#
-#	var childnode = get_children()[0]
-#	childnode.scale.x =
-#	childnode.scale.y = 
-#	var mat = childnode.material_override
-#	
