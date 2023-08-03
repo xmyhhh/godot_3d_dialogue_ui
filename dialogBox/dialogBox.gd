b6844e3c34944c8adf127edb36ca7dc6048166f4
xmyhhh <xmycissy@outlook.com>
2023-08-03 19:51:54 +0800

    udpate

--- dialogBox/dialogBox.gd
+++ dialogBox/dialogBox.gd
@@ -4,7 +4,20 @@ extends Node3D
 @export var top_node:Node
 @export var center_node:Node 
 @export var buttom_node:Node 
+
 @export var reposition = false
+@export var buttom_as_reposition_origin = true
+
+@export var top_text_max_char:int = 8
+@export var center_text_max_char:int = 35
+@export var buttom_text_max_char:int = 4
+
+@export var top_text_reach_max_hide:bool = true
+@export var center_text_reach_max_hide:bool = true
+@export var buttom_text_reach_max_hide:bool = true
+
+@export var center_text_reach_max_auto_scroll:bool = true
+@export var auto_scroll_speed:float = 2.0
 
 var self_init_pos
 var center_init_size
@@ -26,9 +39,9 @@ func _process(delta):
 			self_init_pos = self.transform.origin
 			center_init_size = center_text_node.get_aabb().size
 			top_init_pos = top_node.transform.origin
-			buttom_init_pos = buttom_node.transform.origin
-			
+			buttom_init_pos = buttom_node.transform.origin			
 			return
+			
 		if(reposition):
 			var y_change =  (center_text_node.get_aabb().size.y - center_init_size.y)  / 2.0
 			var x_change =  (center_text_node.get_aabb().size.x - center_init_size.x)  / 2.0
@@ -39,4 +52,13 @@ func _process(delta):
 			self.transform.origin.y = self_init_pos.y + y_change
 
 func set_top(content):
+	top_text_node.text = content
+	pass
+
+func set_center(content):
+	center_text_node.text = content
+	pass
+	
+func set_buttom(content):
+	buttom_text_node.text = content
 	pass
--- test.gd
+++ test.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var dialog_box:Node
+
 var test_data = [
 	{
 		'name': "Mr.Dot",
@@ -7,8 +9,18 @@ var test_data = [
 		'end':"..."
 	},
 	{
-		'name': "Mr.Dot",
-		'content':"dsadasfas asdasdas fsad a",
+		'name': "Mr.Dotasasad",
+		'content':" 三千年前，神明降临。
+					仙、神决战致天地大灾变。灵气衰弱，仙人消失，神明却重新渗透人间；
+					一百六十多年前，异宝降临。
+					富饶的赤帕高原、战无不胜的盘龙古城，尽数化为黄土；
+					如今，我，降临了！
+					追寻仙人留下的线索，扭转既定的宿命，解开仙魔的真相……
+					而一切的起源，要从回到盘龙古城、体验波澜悲壮的历史开始。
+					PS：本文为迷雾文，不开上帝视角，世界的全貌及真相随主角行动而逐步揭开，重探索、重体验，不套路。
+					（已有完结老书三本1200万+字。《保卫国师大人》实体书全网销售，动漫版权售出，影视剧备案已过，筹拍中；
+					《宁小闲御神录》影视、动漫、舞台剧版权均已售出。）
+					",
 		'end':"..."
 	},
 	{
@@ -18,7 +30,17 @@ var test_data = [
 	}
 ]
 
+func test1():
+	dialog_box.set_top(test_data[0]["name"])
+	dialog_box.set_center(test_data[0]["content"])
+	dialog_box.set_buttom(test_data[0]["end"])
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func test2():
+	dialog_box.set_top(test_data[1]["name"])
+	dialog_box.set_center(test_data[1]["content"])
+	dialog_box.set_buttom(test_data[1]["end"])
+
+func test3():
+	dialog_box.set_top(test_data[2]["name"])
+	dialog_box.set_center(test_data[2]["content"])
+	dialog_box.set_buttom(test_data[2]["end"])
